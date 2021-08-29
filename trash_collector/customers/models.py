181bf5ba3ade458e61b28cfa0fbc0a3fe8aaa5b2
Vernon Roberts <nakkiasr@live.com>
2021-08-29 00:07:39 -0700

    Still unable to get this thing to work. Unable to get the db to work, the links...not sure what the issue is.

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.fields import CharField, DateField, IntegerField
+from django import utils
+import django.utils.timezone 
+
 # Create your models here.
 
 # TODO: Finish customer model by adding necessary properties to fulfill user stories
@@ -7,3 +11,17 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    street_address = models.CharField(blank=True, max_length=50)
+    zip_code = models.CharField('customer.Zip Code', max_length=5)
+    amount = models.IntegerField()
+    weekly_pickup = models.CharField(default=True, null=True, max_length=50)
+    one_time_pickup = models.DateField(default=django.utils.timezone.now, null=True)
+    suspend_start = models.DateField(default=django.utils.timezone.now, null=True)
+    suspend_end = models.DateField(default=django.utils.timezone.now, null=True)
+
+    def __str__(self): 
+        return self.name
+    
+    
+ 
+ 
--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.apps import apps
+from django.shortcuts import get_object_or_404
 from .models import Customer
 # Create your views here.
 
@@ -8,16 +12,33 @@ from .models import Customer
 
 def index(request):
     # The following line will get the logged-in in user (if there is one) within any view function
-    user = request.user
+    all_customers = Customer.objects.all()
+    user = request.user()
+    context = {
+        'all_customers': all_customers
+    }
 
     try:
         # This line inside the 'try' will return the customer record of the logged-in user if one exists
         logged_in_customer = Customer.objects.get(user=user)
     except:
         # TODO: Redirect the user to a 'create' function to finish the registration process if no customer record found
-        pass
-
+        
+        return HttpResponseRedirect(reverse('customers:index.html', context))
     # It will be necessary while creating a Customer/Employee to assign request.user as the user foreign key
-
-    print(user)
-    return render(request, 'customers/index.html')
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        street_address = request.POST.get('street_address')
+        postal_code = request.POST.get('postal_code')
+        weekly_pickup = request.POST.get('weekly_pickup')
+        one_time_pickup = request.POST.get('one_time_pickup')
+        suspend_start = request.POST.get('suspend_start')
+        suspend_end = request.POST.get('suspend_end')
+        new_customers = Customer(name=name,  street_address=street_address, postal_code=postal_code, weekly_pickup=weekly_pickup, one_time_pickup=one_time_pickup, suspend_start=suspend_start, suspend_end=suspend_end)
+        new_customers.save()
+        return HttpResponseRedirect(reverse('customers:index'))
+    else:
+        # print(user)
+        return render(request, 'customers/index.html')
+        
